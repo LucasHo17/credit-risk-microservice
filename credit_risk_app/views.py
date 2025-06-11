4fa5881c47b7f7685e6b95d7e433db06b292767c
Ngan Nguyen <nhnguyen@umass.edu>
2025-06-11 10:58:09 +0700

    update views.py with home view

--- credit_risk_app/views.py
+++ credit_risk_app/views.py
@@ -6,6 +6,8 @@ from rest_framework import status
 from credit_risk_app.serializers import RiskMetricsSerializer
 from credit_risk_app.services.capital_calculator import calculate_capital
 
+from django.http import HttpResponse
+
 # Create your views here.
 class CapitalCalculationView(APIView):
     def post(self, request):
@@ -14,3 +16,6 @@ class CapitalCalculationView(APIView):
             result = calculate_capital(serializer.validated_data)
             return Response(result, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def home(request):
+    return HttpResponse("Hello from the Homepage!")
